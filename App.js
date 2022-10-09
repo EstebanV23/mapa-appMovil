15d74116795ce492690025c84887b2b56df5e48c
brayan pc <Esteban.bevf@gmail.com>
2022-10-09 13:32:44 -0500

    Agregando la 2da y 3ra diapositiva al proyecto, sin errores, se muestra el modal en la aplicacion

--- App.js
+++ App.js
@@ -1,28 +1,58 @@
 /* IMPORTAMOS TODAS LAS LIBRERIAS DE 'reaact' */
 import * as React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Modal } from 'react-native';
 import MapView from 'react-native-maps';
 
 export default function App() {
-  return(
-    /* ENVOLVEMOS AL MapView DENTRO DE UN CONTENEDOR PARA ESTIRIZARLOS */
-    <View style={styles.container}>
-      <MapView style={styles.map} />
-    </View>
-  )
+    return (
+        /* ENVOLVEMOS AL MapView DENTRO DE UN CONTENEDOR PARA ESTIRIZARLOS */
+        <View style={styles.container}>
+            <MapView style={styles.map} />
+            {/* MODAL */}
+            <Modal
+                animationType='slide'
+                transparent={true}
+                visible={true}
+            >
+                <View style={styles.center}>
+                    <View style={styles.modalView}>
+                        <Text>Hola</Text>
+                    </View>
+                </View>
+            </Modal>
+        </View>
+    )
 }
 
 /* ESTILOS */
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    /* ESTILOS PARA EL MAPA Y SU CONTAINER */
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+
+    /* ESTILOS PARA EL MODAL Y CENTER */
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalView: {
+        backgroundColor: '#fff',
+        borderRadius: 4,
+        padding: 20,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        }
+    }
 });
